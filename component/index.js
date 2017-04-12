6d4d1a6b355bbe447d5e123ec894d69175538096
jaghit <iamjaghitsingh@gmail.com>
2017-04-12 16:38:59 +0530

    Loading screen.

--- component/index.js
+++ component/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import {
-    Text
+    Image,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
 export default class Index extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            loading: true
+        }
+    }
     render() {
+        setTimeout(() => {
+            this.setState({
+                loading: false
+            });
+        }, 3000);
         return (
-            <Text>Hello World</Text>
+            <View style={styles.container}>
+                {this.state.loading ? <Image style={styles.image} source={require('../asset/loading.png')} /> : <Text style={styles.hello}>Hello World</Text>}
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center'
+    },
+    image: {
+        width: 400,
+        height: 600
+    },
+    hello: {
+        fontSize: 16
+    }
+});
