add02d8711ebefc03db59df38918e4e2d6f60c8c
jaghit <iamjaghitsingh@gmail.com>
2017-04-13 16:09:27 +0530

    Record mail & pass for validation.

--- component/login/index.js
+++ component/login/index.js
@@ -7,21 +7,28 @@ import {
 } from 'react-native';
 
 export default class Login extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            mail: '',
+            pass: ''
+        };
+    }
     render() {
         return (
             <View style={{backgroundColor:'white', flex:1, justifyContent:'center'}}>
-                <Text style={{fontSize:48, fontFamily: 'cursive_bold', marginLeft:48, marginBottom:32}}>Sign in</Text>
-                <Text style={{fontSize:16, marginLeft:64}}>Email address</Text>
-                <TextInput style={{marginLeft:64, width:256}}></TextInput>
-                <Text style={{fontSize:16, marginLeft:64}}>Password</Text>
-                <TextInput style={{marginLeft:64, width:256}}></TextInput>
-                <TouchableHighlight style={{alignItems:'center', backgroundColor:'lightgreen', marginLeft:64, padding:16, width:256}} underlayColor="limegreen" onPress={this.checkSubmit}>
-                    <Text style={{fontSize:16}}>Submit</Text>
+                <Text style={{fontSize:48, fontFamily: 'cursive_bold', marginLeft:32, marginBottom:32}}>Sign in</Text>
+                <Text style={{fontSize:16, marginLeft:48}}>Email address</Text>
+                <TextInput onChangeText={(mail) => this.setState({mail})} style={{marginLeft:64, width:256}} ></TextInput>
+                <Text style={{fontSize:16, marginLeft:48}}>Password</Text>
+                <TextInput onChangeText={(pass) => this.setState({pass})} style={{marginLeft:64, width:256}} ></TextInput>
+                <TouchableHighlight style={{alignItems:'center', backgroundColor:'lightgreen', marginLeft:64, padding:16, width:256}} underlayColor="limegreen" onPress={this.checkSubmit.bind(this)}>
+                    <Text style={{color:'white', fontSize:16}}>Submit</Text>
                 </TouchableHighlight>
             </View>
         );
     }
     checkSubmit() {
-
+        console.log(this.state.mail + " " + this.state.pass);
     }
 }
