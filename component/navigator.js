b2a74e045955b4f460e51847ac894bc25204e2ed
akshay1293 <akshayaggarwal1293@gmail.com>
2017-04-14 13:55:24 +0530

    included navigator component

---
+++ component/navigator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+
+    View,
+    Navigator,
+} from 'react-native';
+
+import Login from './login/index';
+import Here from './login/here';
+
+export default class Nav extends React.Component {
+
+    renderScene(route, navigator) {
+
+        switch (route.id) {
+
+            case 'login':
+                return <Login route={route} navigator={navigator} />
+            case 'next':
+                <Here route={route} navigator={navigator} />
+        }
+    }
+
+
+    render() {
+
+        return (
+
+            <Navigator
+                initialRoute={{ id: 'login' }}
+                renderScene={this.renderScene.bind(this)} />
+
+
+        );
+    }
+
+}
