5c96cb304f7ade19d4a1ce042188ea0eb9d165c5
akshay1293 <akshayaggarwal1293@gmail.com>
2017-04-19 14:28:37 +0530

    new component

---
+++ component/login/enterName.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Text,
+    TextInput,
+    View,
+    StyleSheet,
+} from 'react-native';
+
+export default class EnterName extends React.Component {
+
+    render() {
+
+        return (
+
+            <View>
+
+            </View>
+        );
+    }
+}
