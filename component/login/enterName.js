60f6e5de94788e416f7ff62d1f6d665051cf8bc8
akshay1293 <akshayaggarwal1293@gmail.com>
2017-04-19 15:30:07 +0530

    component created

--- component/login/enterName.js
+++ component/login/enterName.js
@@ -4,6 +4,7 @@ import {
     TextInput,
     View,
     StyleSheet,
+    TouchableHighlight
 } from 'react-native';
 
 export default class EnterName extends React.Component {
@@ -12,9 +13,30 @@ export default class EnterName extends React.Component {
 
         return (
 
-            <View>
+            <View style={styles.container}>
+                <Text style={{ fontSize: 16, marginLeft: 48 }}>Enter Name</Text>
+                <TextInput style={styles.inputName} maxLength={10}></TextInput>
+                <TouchableHighlight style={{ alignItems: 'center', backgroundColor: 'lightgreen', marginLeft: 48, padding: 16, width: 256 }} underlayColor="limegreen">
+                    <Text style={{ color: 'white', fontSize: 16 }}>Finish</Text>
+                </TouchableHighlight>
 
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+
+        flex: 1,
+
+        justifyContent: 'center'
+    },
+    inputName: {
+        marginLeft: 48,
+        width: 256,
+        fontSize: 17,
+
+    }
+})
